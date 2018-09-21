383a977af38766b34691a61771c679f857c5e14d
James <jamescraig@finervision.com>
2018-09-21 22:07:41 +0100

    LoginController is now validated

--- src/server/Controllers/LoginController.js
+++ src/server/Controllers/LoginController.js
@@ -1,20 +1,24 @@
 import bcrypt from "bcrypt";
 import FindUser from "../Functions/FindUser";
+import app from "../app/index"
+import config from "../../common/config";
 
 const unauthorized = (req, res) => {
     console.error(`Unauthorized access for "${req.body.name}"`);
 
     res.status(401);
-    res.send(JSON.stringify({success: false, errors: ['Incorrect login, try again']}));
+    res.send(JSON.stringify({success: false, errors: ['Incorrect credentials']}));
 };
 
 export default async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    // Validate data exists
-    if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('password')) {
+    const validation = app.Validator.validate(req.body, config.validators.login(req.body));
+
+    // Validate validation passed
+    if (!validation.passed) {
         res.status(400);
-        res.send(JSON.stringify({success: false, errors: ['Enter a name and password']}));
+        res.send(JSON.stringify({success: false, errors: validation.getErrors()}));
         return;
     }
 
