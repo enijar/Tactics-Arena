bfad20a85118b569c6487a1647b4e1fcf2b1bb86
James <jamescraig@finervision.com>
2018-09-27 21:51:59 +0100

    Register function now sets jwt on user

--- src/server/app/Functions/Auth/Register.js
+++ src/server/app/Functions/Auth/Register.js
@@ -3,6 +3,10 @@ import FindUser from "../DB/FindUser";
 import app from "../../index"
 import config from "../../../../common/config";
 import CreateUser from "../DB/CreateUser";
+import SaveUserToken from "./SaveUserToken";
+import RemoveHiddenFields from "../DB/RemoveHiddenFields";
+import jwt from "jsonwebtoken";
+import env from "../../../../../env";
 
 const SALT_ROUNDS = 10;
 
@@ -30,11 +34,15 @@ export default async data => {
     // Store name and hashed password in DB
     try {
         const password = await bcrypt.hash(data.password, SALT_ROUNDS);
-        await CreateUser({name: data.name, password});
+        let user = await CreateUser({name: data.name, password});
+        user = RemoveHiddenFields(user, ['password']);
+        user.jwt = jwt.sign({user}, env.jwt.secret);
+        SaveUserToken(user);
 
         return {
             success: true,
-            status: 200
+            status: 200,
+            user
         }
     } catch (err) {
         console.error(`Registration error: "${err.message}"`);
--- src/server/app/Functions/DB/CreateUser.js
+++ src/server/app/Functions/DB/CreateUser.js
@@ -1,11 +1,11 @@
 import Query from "./Query";
 
 export default async user => {
+    const data = [user.name, user.password];
+
     try {
-        await Query("insert into users (name, password, created_at, updated_at) values (?, ?, NOW(), NOW())", [
-            user.name,
-            user.password
-        ]);
+        await Query("insert into users (name, password, created_at, updated_at) values (?, ?, NOW(), NOW())", data);
+        return await Query('select * from users where name = ? and password = ? limit 1', data);
     } catch (err) {
         console.error(`CreateUser query failed: "${err.message}"`);
     }
