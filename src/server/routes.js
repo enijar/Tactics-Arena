fb9f436afcf1f8ccff6c408e6a5f8371a18561d2
James <jamescraig@finervision.com>
2018-10-06 22:58:51 +0100

    Removed PlayerListController

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,12 +1,10 @@
 const AppController = require('./controllers/AppController');
 const GameListController = require('./controllers/GameListController');
-const PlayerListController = require('./controllers/PlayerListController');
 const LoginController = require('./controllers/LoginController');
 const RegisterController = require('./controllers/RegisterController');
 
 module.exports = app => {
     app.get('/api/game-list', GameListController);
-    app.get('/api/player-list', PlayerListController);
     app.post('/api/login', LoginController);
     app.post('/api/register', RegisterController);
     app.get('*', AppController);
