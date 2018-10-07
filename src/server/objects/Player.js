aa84aa18bd24f1c3c6d41e068d4ee3b6eb3615ae
James <jamescraig@finervision.com>
2018-10-07 12:56:28 +0100

    Added more comments and logs to Player object

--- src/server/objects/Player.js
+++ src/server/objects/Player.js
@@ -1,15 +1,25 @@
+const Logger = require('../functions/Logger');
 const state = require('../state/index');
 const config = require('../config/index');
 
+/**
+ * Object instance of a connected player.
+ *
+ * @type {module.Player}
+ */
 module.exports = class Player {
     constructor(props) {
         Object.assign(this, props);
         this.idleTimeout = null;
-
         this.resetIdleTimeout();
     }
 
+    /**
+     * Reset the timeout, indicating if this user is idle.
+     */
     resetIdleTimeout() {
+        Logger.info(`Player.resetIdleTimeout(active) -> ${this.name}`);
+
         for (let i = 0; i < state.players.length; i++) {
             if (state.players[i].name === this.name) {
                 state.players[i].status = 'active';
@@ -21,6 +31,8 @@ module.exports = class Player {
 
         this.idleTimeout && clearTimeout(this.idleTimeout);
         this.idleTimeout = setTimeout(() => {
+            Logger.info(`Player.resetIdleTimeout(idle) -> ${this.name}`);
+
             for (let i = 0; i < state.players.length; i++) {
                 if (state.players[i].name === this.name) {
                     state.players[i].status = 'idle';
@@ -31,11 +43,20 @@ module.exports = class Player {
         }, config.player.idleTimeout);
     }
 
+    /**
+     * Clear timeouts and emit the players.update event.
+     */
     destroy() {
+        Logger.info(`Player.destroy -> ${this.name}`);
         this.idleTimeout && clearTimeout(this.idleTimeout);
         this.io.emit('players.update', state.players.map(player => player.get()));
     }
 
+    /**
+     * Publicly visible data
+     *
+     * @returns {Object}
+     */
     get() {
         return {
             id: this.id,
