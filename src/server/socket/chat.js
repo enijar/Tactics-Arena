76a1602f71f2d7c3d846b7d7e99e5b3297976db3
James <jamescraig@finervision.com>
2018-10-07 13:36:56 +0100

    Added logs to chat socket and emit user object with "chat.message"

--- src/server/socket/chat.js
+++ src/server/socket/chat.js
@@ -1,15 +1,17 @@
+const Logger = require('../functions/Logger');
+const state = require('../state/index');
+
 module.exports = (io, socket) => {
     socket.on('chat.message', data => {
         const date = new Date();
         const time = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 
+        Logger.info(`Message from "${data.user.name}" saying "${data.message}"`);
+
         io.emit('chat.message', {
             time,
             message: data.message,
-            user: {
-                name: data.user.name,
-                type: data.user.type
-            }
+            user: state.players[socket.id].get()
         });
     });
 };
