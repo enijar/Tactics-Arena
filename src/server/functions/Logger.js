560ddd818f28c1a336a786d6dcea3846c73917b4
James <jamescraig@finervision.com>
2018-10-07 12:52:58 +0100

    Added timestamp to custom Logger function

--- src/server/functions/Logger.js
+++ src/server/functions/Logger.js
@@ -2,19 +2,24 @@ const path = require('path');
 const winston = require('winston');
 const config = require('../config/index');
 
+const format = winston.format.printf(info => {
+    return `${info.timestamp} [${info.level}]: ${info.message}`;
+});
+
 module.exports = winston.createLogger({
-    format: winston.format.simple(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        format
+    ),
     transports: [
         new winston.transports.Console({
-            handleExceptions: true,
-            colorize: true
+            handleExceptions: true
         }),
         new winston.transports.File({
             filename: path.join(config.paths.storage, 'app.log'),
             handleExceptions: true,
             maxsize: 5242880, // 5MB
             maxFiles: 2,
-            colorize: false,
             tailable: true
         })
     ]
