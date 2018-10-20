8672b485e5ccf31191f99fbb8d64b6e4720a9aaf
James <jamescraig@finervision.com>
2018-10-20 19:44:29 +0100

    Changed model download maths

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -4,5 +4,5 @@
 
 # Furgon model
 mkdir -p public/models/furgon
-curl -o public/models/furgon/model.dae https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/model.dae
-curl -o public/models/furgon/skin.png https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/skin.png
+curl -o public/models/furgon/Furgon_Model.fbx https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/Furgon_Model.fbx
+curl -o public/models/furgon/Furgon_Model_final.png https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/Furgon_Model_final.png
