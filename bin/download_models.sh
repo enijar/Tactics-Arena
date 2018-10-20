98d172c7f057055a75b3086c9ba6e0415d829315
James <jamescraig@finervision.com>
2018-10-20 17:23:28 +0100

    Fixed model skin typo

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -5,4 +5,4 @@
 # Furgon model
 mkdir -p public/models/furgon
 curl -o public/models/furgon/model.dae https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/model.dae
-curl -o public/models/furgon/skin.dae https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/skin.dae
+curl -o public/models/furgon/skin.png https://s3.eu-west-2.amazonaws.com/arenareborn/models/furgon/skin.png
