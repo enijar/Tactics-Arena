9240a18592a189a6c72271a5314f464a2cbdd3be
James <jamescraig@finervision.com>
2018-10-28 14:28:26 +0000

    Updated chat component
    • Added gold and gray colours to chat text and player name
    • Cleaned up MessageParser class

--- src/server/app/Chat/MessageParser.js
+++ src/server/app/Chat/MessageParser.js
@@ -1,47 +1,47 @@
 const commands = require('./Commands/index');
 
+const returnCommand = text => ({type: 'command', text});
+
 module.exports = class MessageParser {
-    constructor(player, message) {
+    constructor(player, message = '') {
         this.player = player;
-        this.message = message;
+        this.message = String(message);
     }
 
     async parse() {
-        let type = 'default';
-        let text = String(this.message || '');
-
-        if (text.startsWith('/')) {
-            type = 'command';
-            const parsed = /^\/(\w+)\s?(.*)/.exec(text);
-            const command = parsed[1];
-            let args = String(parsed[2] || '');
-            args = !args ? [] : args.split(' ');
-
-            if (!command) {
-                return {type, text: 'Invalid command'};
-            }
-
-            let chatCommand = null;
-
-            // Get matching commandTest RegExp command class instance
-            for (let commandTest in commands) {
-                if (!commands.hasOwnProperty(commandTest)) {
-                    continue;
-                }
-
-                if (command.match(new RegExp(commandTest))) {
-                    chatCommand = new commands[commandTest](this.player, args);
-                    break;
-                }
-            }
-
-            if (!chatCommand) {
-                return {type, text: 'Invalid command'};
-            }
-
-            text = await chatCommand.run();
+        if (!this.message.startsWith('/')) {
+            return {
+                type: this.player.type,
+                text: this.message,
+            };
         }
 
-        return {type, text};
+        const parsed = /^\/(\w+)\s?(.*)/.exec(text);
+        const command = parsed[1];
+        const args = !parsed[2] ? [] : parsed[2].split(' ');
+
+        if (!command) {
+            return returnCommand('Invalid command');
+        }
+
+        let chatCommand = null;
+
+        // Get command class from matching command RegExp test keys
+        for (let commandTest in commands) {
+            if (!commands.hasOwnProperty(commandTest)) {
+                continue;
+            }
+
+            if (command.match(new RegExp(commandTest))) {
+                chatCommand = new commands[commandTest](this.player, args);
+                break;
+            }
+        }
+
+        if (!chatCommand) {
+            return returnCommand('Invalid command');
+        }
+
+        return returnCommand(await chatCommand.run());
     }
 };
