3f314c784a17918db2f05050ed81cfea2dd5dd73
James <jamescraig@finervision.com>
2018-10-28 14:31:18 +0000

    Added doc blocks to Command class

--- src/server/app/Chat/Commands/Command.js
+++ src/server/app/Chat/Commands/Command.js
@@ -4,10 +4,25 @@ module.exports = class Command {
         this.args = args;
     }
 
+    /**
+     * Execute this command and wait for the message value to
+     * be return.
+     *
+     * @returns {Promise<string>}
+     */
     async run() {
         return '';
     }
 
+    /**
+     * Get the arg value from the args array by the given
+     * index or return the given default value if no arg
+     * exists in the args array.
+     *
+     * @param {Number} index
+     * @param {*|null} defaultValue
+     * @returns {*|defaultValue}
+     */
     getArg(index, defaultValue = null) {
         if (!this.args[index]) {
             return defaultValue;
